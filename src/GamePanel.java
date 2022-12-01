63e8f7f0a13f2f22e7cdf5a00131b8e80f641e62
joshuahT <joshuahtangs@gmail.com>
2022-12-01 00:14:38 -0500

    Add Body to Snake Component, Made head green and body blue

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -50,6 +50,18 @@ public class GamePanel extends JPanel implements ActionListener {
         }
         g.setColor(Color.RED);
         g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+
+        for(int i = 0; i < bodyParts; i++){
+            if(i == 0){
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE,UNIT_SIZE);
+
+            }
+            else{
+                g.setColor(Color.BLUE);
+                g.fillRect(x[i], y[i], UNIT_SIZE,UNIT_SIZE);
+            }
+        }
     }
     public void newApple(){
         appleX = random.nextInt((int)SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
@@ -57,6 +69,27 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
     public void move(){
+
+        for(int i = bodyParts; i>0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+
+        }
+        switch(direction){
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
+
 
     }
     public void checkPoints(){
